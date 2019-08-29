993df71e55e019d98616543debfe84fb8e13c22d
dmlstd <2500427706@qq.com>
2019-08-29 21:44:15 +0800

    dml commit 8-29 21:41 影院模块逆向生成service层

---
+++ guns-cinema/src/main/java/com/stylefeng/guns/cinema/persistence/dao/MtimeHallFilmInfoTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.cinema.persistence.dao;
+
+import com.stylefeng.guns.cinema.persistence.model.MtimeHallFilmInfoT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影厅电影信息表 Mapper 接口
+ * </p>
+ *
+ * @author dml
+ * @since 2019-08-29
+ */
+public interface MtimeHallFilmInfoTMapper extends BaseMapper<MtimeHallFilmInfoT> {
+
+}
---
+++ guns-cinema/src/main/java/com/stylefeng/guns/cinema/persistence/service/impl/MtimeBrandDictTServiceImpl.java
@@ -0,0 +1,20 @@
+package com.stylefeng.guns.cinema.persistence.service.impl;
+
+import com.stylefeng.guns.cinema.persistence.model.MtimeBrandDictT;
+import com.stylefeng.guns.cinema.persistence.dao.MtimeBrandDictTMapper;
+import com.stylefeng.guns.cinema.persistence.service.IMtimeBrandDictTService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 品牌信息表 服务实现类
+ * </p>
+ *
+ * @author dml
+ * @since 2019-08-29
+ */
+@Service
+public class MtimeBrandDictTServiceImpl extends ServiceImpl<MtimeBrandDictTMapper, MtimeBrandDictT> implements IMtimeBrandDictTService {
+
+}
