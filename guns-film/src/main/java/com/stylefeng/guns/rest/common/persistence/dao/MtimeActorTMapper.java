2264aaf289790997dc26af3afcd65b66cea6e68c
taotaoing <1906371514@qq.com>
2019-08-29 22:01:16 +0800

    新建film

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MtimeActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MtimeActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author stylefeng
+ * @since 2019-08-29
+ */
+public interface MtimeActorTMapper extends BaseMapper<MtimeActorT> {
+
+}
